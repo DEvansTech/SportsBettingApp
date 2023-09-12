a86836e3c71346003ca0d585872f22ed7d9b181c
SoftwareGuru <wizard.man0308@gmail.com>
2023-09-12 11:28:11 -0400

    Updated carousel page

--- src/scenes/Introduction/Scoreboard.tsx
+++ src/scenes/Introduction/Scoreboard.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Container, Content, Text, View, Icon, Header } from 'native-base';
+import { Container, Content, Text, View, Header, Icon } from 'native-base';
 import { SvgXml } from 'react-native-svg';
 
 import { LoadingImage } from '@Components';
 import { Colors, Images, Svgs } from '@Theme';
-import { CarosuelProps } from './types';
 import styles, { scale } from './styles';
+import { CarosuelProps } from './types';
 
-const OBISays: React.FC<CarosuelProps> = ({
+const Scoreboard: React.FC<CarosuelProps> = ({
   nextPage,
   prevPage,
   closePage
@@ -20,7 +20,7 @@ const OBISays: React.FC<CarosuelProps> = ({
         androidStatusBarColor={Colors.green}>
         <View style={styles.headerView}>
           <Text style={styles.headerSubTitle}>Instructions:</Text>
-          <Text style={styles.headerTitle}>Roi Says</Text>
+          <Text style={styles.headerTitle}>Scoreboard</Text>
           <Icon
             type="AntDesign"
             name="closecircleo"
@@ -46,20 +46,32 @@ const OBISays: React.FC<CarosuelProps> = ({
       <Content contentContainerStyle={styles.contentView}>
         <View style={styles.body}>
           <View style={styles.firstView}>
-            <LoadingImage source={Images.carousel5} style={styles.imageView} />
+            <LoadingImage source={Images.carousel7} style={styles.imageView1} />
           </View>
           <View style={styles.secondView}>
-            <SvgXml
-              xml={Svgs.obiIcon}
-              width={60 * scale}
-              height={60 * scale}
-              style={styles.mainIcon}
-            />
             <Text style={styles.normalText}>
-              Meet Roi (pronounced "Roy"), job title Figurehead, who explains
-              all things betting in a series of commentaries with the newest on
-              top. If you’ve just joined, scroll down to Roi #1 to start at the
-              beginning.
+              To see how our green “value” plays are doing, you’ll find{' '}
+              <Text style={styles.blackBoldText}>SCOREBOARD</Text> (a coming
+              attraction) under the{' '}
+              <Text style={styles.blackBoldText}>“WATCHING”</Text> tab.
+            </Text>
+          </View>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.normalText}>
+            Instead of just wins and losses, we use “Return On Investment” to
+            account for differences in odds — favorites and longshots.
+          </Text>
+        </View>
+
+        <View style={styles.obisaysView}>
+          <SvgXml xml={Svgs.obiIcon} width={58 * scale} height={58 * scale} />
+          <Text>.....</Text>
+          <View style={styles.obisaysContent}>
+            <Text style={styles.whiteBoldText}>
+              OddsR™ uses closing sportsbook lines on the day of the game to
+              make this ROI calculation. But you can also find green value plays
+              on the days before to “lock in” a good price.
             </Text>
           </View>
         </View>
@@ -68,4 +80,4 @@ const OBISays: React.FC<CarosuelProps> = ({
   );
 };
 
-export default OBISays;
+export default Scoreboard;
